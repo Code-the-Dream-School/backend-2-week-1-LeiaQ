5372571bb7cac17be52c5f88972a40248a7f9281
Leia Quesada <47373912+LeiaQ@users.noreply.github.com>
2020-10-31 12:08:28 -0700

    assigned instance variable to handle pages, added ERB code to list each page title, debug NameError in PagesController#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     #manually create this action method
     def index
-        #assign instance variable
-        @pages = Pages.all
+        #assign instance variable to handle pages
+        @pages = Page.all
     end
 end
