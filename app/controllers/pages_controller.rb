8eb36886707a53a4a975bb5b8cf099f8c22e3958
Leia Quesada <47373912+LeiaQ@users.noreply.github.com>
2020-10-31 12:58:31 -0700

    generated pages in model directory and migrated to fix uninitialized constant page bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
-    #manually create this action method
     def index
-        #assign instance variable to handle pages
         @pages = Page.all
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201019064934) do
+ActiveRecord::Schema.define(version: 20201031195415) do
 
   create_table "models", force: :cascade do |t|
     t.string   "Page"
+    t.string   "title"
+    t.text     "body"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.string   "slug"
