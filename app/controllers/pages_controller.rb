151e922fa2910809c64492b79efae64d6baf4419
Leia Quesada <47373912+LeiaQ@users.noreply.github.com>
2020-11-04 11:40:19 -0800

    Created show file and route paths for individual blog pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
     def index
         @pages = Page.all
     end
+    def show
+        @page = Page.find(params[:id])
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   # get requests for the /pages path should go to the PagesController's index method
   get '/pages', to: 'pages#index' 
+  # sets up a get route to view an individual page that matches the particular id
+  # :id path is available within the controller as params[:id], this request gets routed to pages controller's show method 
+  get '/pages/:id', to: 'pages#show', as: 'page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
